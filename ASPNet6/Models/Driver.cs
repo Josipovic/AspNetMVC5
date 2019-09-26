978800feb71ac4107c83dd9c8a91bbb1925dd970
Katarina Josipovic <katarina.josipovic@hotmail.com>
2019-09-26 17:34:50 +0200

    first commit

---
+++ ASPNet6/Models/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNet6.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public float Salary { get; set; }
+        public bool Hired { get; set; }
+    }
+}
---
+++ ASPNet6/Models/DriverContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASPNet6.Models
+{
+    public class DriverContext:DbContext
+    {
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
